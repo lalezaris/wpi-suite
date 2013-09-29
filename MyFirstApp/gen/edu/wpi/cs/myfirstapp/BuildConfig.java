30df68890c411528f477fa6b53bf2e60a1c18fc0
lalezaris <lalezaris@wpi.edu>
2013-09-29 17:42:50 -0400

    Names are added to the list

---
+++ MyFirstApp/gen/edu/wpi/cs/myfirstapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.wpi.cs.myfirstapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- MyFirstApp/src/edu/wpi/cs/myfirstapp/MainActivity.java
+++ MyFirstApp/src/edu/wpi/cs/myfirstapp/MainActivity.java
@@ -1,18 +1,40 @@
 package edu.wpi.cs.myfirstapp;
 
-import edu.wpi.cs.myfirstapp.EnterFragment.EnterButtonListener;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
-public class MainActivity extends FragmentActivity implements EnterButtonListener {
+public class MainActivity extends FragmentActivity /*implements EnterButtonListener */{
 
 	public static final String NAME = "edu.wpi.cs.myfirstapp.NAME";
-
+	ListView listView;
+	Button enterButton;
+	EditText  nameField;
+	
+	ArrayAdapter<String> adapter;
+	List<String> names;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        listView = (ListView)findViewById(R.id.log_view);
+        enterButton = (Button)findViewById(R.id.enter_button);
+        nameField = (EditText)findViewById(R.id.name_input);
+        
+        names = new ArrayList<String>();
+        
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+        listView.setAdapter(adapter);
     }
 
 
@@ -24,12 +46,10 @@ public class MainActivity extends FragmentActivity implements EnterButtonListene
     }
 
 
-	@Override
-	public void sendInfo(String name) {
-		
-		LogFragment logFrag = (LogFragment) getSupportFragmentManager().findFragmentById(R.id.log_fragment);
-		
-		logFrag.addName(name);
+	public void sendName(View view) {
+		names.add(nameField.getText().toString());
+		adapter.notifyDataSetChanged();
+		nameField.setText("");
 		
 	}
     
