731ef1a39dcc0a9df018599379e01c8c66e87643
Nathan Longnecker <nathanlongnecker@gmail.com>
2013-10-08 11:25:50 -0400

    Add FragmentTest project that communicates with the core

---
+++ FragmentTest/src/edu/wpi/cs/fragmenttest/MyListFragment.java
@@ -0,0 +1,71 @@
+package edu.wpi.cs.fragmenttest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.util.List;
+import java.util.Map;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+
+public class MyListFragment extends Fragment {
+  
+  private OnItemSelectedListener listener;
+  
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_rsslist_overview,
+        container, false);
+    Button button = (Button) view.findViewById(R.id.button1);
+    button.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        updateDetail();
+      }
+    });
+    return view;
+  }
+
+  public interface OnItemSelectedListener {
+      public void onRssItemSelected(String link);
+    }
+  
+  @Override
+    public void onAttach(Activity activity) {
+      super.onAttach(activity);
+      if (activity instanceof OnItemSelectedListener) {
+        listener = (OnItemSelectedListener) activity;
+      } else {
+        throw new ClassCastException(activity.toString()
+            + " must implemenet MyListFragment.OnItemSelectedListener");
+      }
+    }
+  
+  
+  // May also be triggered from the Activity
+public void updateDetail() {
+    // Create fake data
+    //String newTime = String.valueOf(System.currentTimeMillis());
+    
+    //set url http://localhost:8080/WPISuite/API
+    //set http method
+    //add headers
+    
+	listener.onRssItemSelected(new LoginTask().execute("aa").toString());
+    
+    // Send data to Activity
+  }
+} 
