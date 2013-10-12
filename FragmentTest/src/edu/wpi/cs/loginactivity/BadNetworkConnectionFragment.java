6a673a44c695430c48e5a072a62a1a2db7b247d5
ibbolia <mwfitzgibbon@wpi.edu>
2013-10-12 17:16:10 -0400

    Added functionality to drop login attempt when network is unavailable to device

--- FragmentTest/src/edu/wpi/cs/loginactivity/BadNetworkConnectionFragment.java
+++ FragmentTest/src/edu/wpi/cs/loginactivity/BadNetworkConnectionFragment.java
@@ -3,25 +3,18 @@ package edu.wpi.cs.loginactivity;
 import edu.wpi.cs.fragmenttest.R;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 
 public class BadNetworkConnectionFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage(R.string.dialog_bad_network)
-               .setPositiveButton(R.string.retry, new DialogInterface.OnClickListener() {
+        builder.setMessage(R.string.dialog_bad_network).setPositiveButton(R.string.okay, new DialogInterface.OnClickListener() {
                    public void onClick(DialogInterface dialog, int id) {
-                	   //re-attempt connection
-                   }
-               })
-               .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                   public void onClick(DialogInterface dialog, int id) {
-                       // User cancelled the dialog
-                	   //exit application
+                	   //closes dialog, allows waiting for re-attempt
                    }
                });
         // Create the AlertDialog object and return it
