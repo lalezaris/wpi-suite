686db75970654b486636c457c245d367d1d72989
Nathan Longnecker <nathanlongnecker@gmail.com>
2013-11-25 22:10:45 -0500

    Added eclipse-jar and eclipse-clean commands to common-build.xml. Those commands use the .class files generated by eclipse to make jar files build with the eclipse build. The end result is that users can clean and build once, and have Eclipse rebuild all of the generated jar files in WPISuite WEB-INF.

--- AndroidCalendar/src/edu/wpi/cs/wpisuitetng/apps/calendar/weekview/CalendarWeekViewActivity.java
+++ AndroidCalendar/src/edu/wpi/cs/wpisuitetng/apps/calendar/weekview/CalendarWeekViewActivity.java
@@ -1,22 +1,9 @@
 package edu.wpi.cs.wpisuitetng.apps.calendar.weekview;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.wpi.cs.wpisuitetng.apps.calendar.R;
-import edu.wpi.cs.wpisuitetng.apps.calendar.R.layout;
-import edu.wpi.cs.wpisuitetng.apps.calendar.R.menu;
 import edu.wpi.cs.wpisuitetng.apps.calendar.common.CalendarCommonMenuActivity;
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
 
 public class CalendarWeekViewActivity extends CalendarCommonMenuActivity {
 	
