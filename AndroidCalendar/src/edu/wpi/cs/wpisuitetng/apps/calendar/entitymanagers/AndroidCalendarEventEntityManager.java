91dbdffa8705632282854e3534b495e935c697dd
lalezaris <lalezaris@wpi.edu>
2013-12-08 18:52:22 -0500

    Fixed NoClassDefFound, and view event page now gets the event

--- AndroidCalendar/src/edu/wpi/cs/wpisuitetng/apps/calendar/entitymanagers/AndroidCalendarEventEntityManager.java
+++ AndroidCalendar/src/edu/wpi/cs/wpisuitetng/apps/calendar/entitymanagers/AndroidCalendarEventEntityManager.java
@@ -16,7 +16,6 @@ import edu.wpi.cs.wpisuitetng.exceptions.BadRequestException;
 import edu.wpi.cs.wpisuitetng.exceptions.ConflictException;
 import edu.wpi.cs.wpisuitetng.exceptions.NotFoundException;
 import edu.wpi.cs.wpisuitetng.exceptions.WPISuiteException;
-import edu.wpi.cs.wpisuitetng.marvin.loginactivity.MarvinUserData;
 import edu.wpi.cs.wpisuitetng.modules.EntityManager;
 import edu.wpi.cs.wpisuitetng.modules.Model;
 import edu.wpi.cs.wpisuitetng.modules.core.models.User;
@@ -104,7 +103,7 @@ public class AndroidCalendarEventEntityManager implements EntityManager<Model> {
 		}
 		
 		// Filter out event the current user does not belong to
-		modelList = checkIfIsAttendee(MarvinUserData.getUsername(), modelList);
+		modelList = checkIfIsAttendee(s.getUsername(), modelList);
 		
 		// If appropriate, sort the array before returning
 		Model[] models = modelList.toArray(new Model[0]);
