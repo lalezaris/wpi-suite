13c825a2a5bd185336b8d54794b61f2933c45f16
ibbolia <mwfitzgibbon@wpi.edu>
2013-12-08 18:15:49 -0500

    Boxes scale and react properly to input

--- AndroidCalendar/src/edu/wpi/cs/wpisuitetng/apps/calendar/drawTest/BubbleSurfaceView.java
+++ AndroidCalendar/src/edu/wpi/cs/wpisuitetng/apps/calendar/drawTest/BubbleSurfaceView.java
@@ -1,32 +1,71 @@
 package edu.wpi.cs.wpisuitetng.apps.calendar.drawTest;
 
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import edu.wpi.cs.wpisuitetng.apps.calendar.models.AndroidCalendarEvent;
+import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.View.OnTouchListener;
+
 
 public class BubbleSurfaceView extends SurfaceView  
-implements SurfaceHolder.Callback {
+implements SurfaceHolder.Callback, OnTouchListener {
 	private SurfaceHolder sh;
 	private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+	List<AndroidCalendarEvent> events;
+	List<EventSquare> squares = new ArrayList<EventSquare>();
+	
 	public BubbleSurfaceView(Context context) {
 		super(context);
 		sh = getHolder();
 		sh.addCallback(this);
 		paint.setColor(Color.BLUE);
 		paint.setStyle(Style.FILL);
+		
+		events = new ArrayList<AndroidCalendarEvent>();
+
+		events.add(new AndroidCalendarEvent("Pizza party", new GregorianCalendar(2013, 12, 2, 15, 0),
+				new GregorianCalendar(2013, 12, 2, 15, 30), "HERE", new ArrayList<User>(),
+				new GregorianCalendar(), null, "Things"));
+
+		events.add(new AndroidCalendarEvent("Ice Cream party", new GregorianCalendar(2013, 12, 2, 6, 0),
+				new GregorianCalendar(2013, 12, 2, 9, 0), "HERE", new ArrayList<User>(),
+				new GregorianCalendar(), null, "Things"));
+
+		events.add(new AndroidCalendarEvent("MQP Meeting", new GregorianCalendar(2013, 12, 2, 3, 0),
+				new GregorianCalendar(2013, 12, 2, 4, 0), "HERE", new ArrayList<User>(),
+				new GregorianCalendar(), null, "Things"));
+
+		events.add(new AndroidCalendarEvent("Last Day of Classes", new GregorianCalendar(2013, 12, 2, 11, 0),
+				new GregorianCalendar(2013, 12, 2, 13, 0), "HERE", new ArrayList<User>(),
+				new GregorianCalendar(), null, "Things"));
+		
 	}
 	public void surfaceCreated(SurfaceHolder holder) {
+		this.setOnTouchListener(this);//listens to itself
 		Canvas canvas = sh.lockCanvas();
-		
-		EventSquare sq = new EventSquare();
-		this.setOnTouchListener(sq);
 		canvas.drawColor(Color.WHITE);
+		
+		for(AndroidCalendarEvent e : this.events){
+			EventSquare sq = new EventSquare(e, this);
+			
+			squares.add(sq);
+			sq.getShape().draw(canvas);//draws shape inside EventSquare objects
+		}
+		
+		
 		//canvas.drawCircle(100, 200, 50, paint);
-		sq.getShape().draw(canvas);//draws shape inside EventSquare objects
+		
 		sh.unlockCanvasAndPost(canvas);
 	}
 	public void surfaceChanged(SurfaceHolder holder, int format, int width,
@@ -34,4 +73,15 @@ implements SurfaceHolder.Callback {
 	}
 	public void surfaceDestroyed(SurfaceHolder holder) {
 	}
+	@Override
+	public boolean onTouch(View arg0, MotionEvent arg1) {
+		for(EventSquare s : squares){
+			if(s.handleTouch(arg0, arg1)){
+				return true;//end loop on first correct touch
+			}
+		}
+		return false;
+	}
+	
+	
 }
--- AndroidCalendar/src/edu/wpi/cs/wpisuitetng/apps/calendar/drawTest/EventSquare.java
+++ AndroidCalendar/src/edu/wpi/cs/wpisuitetng/apps/calendar/drawTest/EventSquare.java
@@ -1,27 +1,64 @@
 package edu.wpi.cs.wpisuitetng.apps.calendar.drawTest;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import edu.wpi.cs.wpisuitetng.apps.calendar.models.AndroidCalendarEvent;
 import android.graphics.Color;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.RectShape;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.View.OnTouchListener;
 
-public class EventSquare implements OnTouchListener{
-	
+public class EventSquare {
+
 	private ShapeDrawable shape;
 	private AndroidCalendarEvent event;
-	int x, y, width, height;
-	
-	public EventSquare(){
+	int x1, y1, x2, y2;
+
+	public EventSquare(AndroidCalendarEvent ev, View v){
+		this.event = ev;
+		//use time & screen res to set height
+		//width based on screen width
+		this.x1 = 0;
+		this.x2 = v.getWidth();
+
+		//get start time
+		//set top of square
+		//get end time
+		//set bottom of square
+		//
+
+		//from start time, figure out top of day
+		GregorianCalendar testCompare = new GregorianCalendar(2013, 12, 2, 0, 0);
+		GregorianCalendar testCompareEnd = new GregorianCalendar(2013, 12, 3, 0, 0);
+		long epochOfDay = testCompare.getTimeInMillis();
+		long endOfDay = testCompareEnd.getTimeInMillis();
+		long millisPerPixel =  (endOfDay - epochOfDay)/v.getHeight();
+		System.out.println("Milliseconds per pixel: " + millisPerPixel);
+
+		if(event.getStartDateAndTime().before(testCompare)){
+			this.y1 = 0;
+		}
+		else{
+			this.y1 = (int) ((event.getStartDateAndTime().getTimeInMillis() - epochOfDay) / millisPerPixel);
+
+		}
+		//	y1 = 0;
+
+		if(ev.getEndDateAndTime().after(testCompareEnd)){
+			this.y2 = v.getHeight();
+		}
+		else{
+			this.y2 = (int) ((event.getEndDateAndTime().getTimeInMillis() - epochOfDay) / millisPerPixel);
+		}
+
 		shape = new ShapeDrawable(new RectShape());
-		shape.getPaint().setColor(Color.RED);
-		shape.setBounds(10, 10, 300, 300);
-		this.x = 10;
-		this.y = 10;
-		this.width = 300-10;
-		this.height = 300-10;
+		shape.getPaint().setColor(Color.BLUE);
+		shape.setBounds(x1, y1, x2, y2);
+		System.out.println("Height is " + (this.y2 - this.y1));
+
 	}
 
 	public ShapeDrawable getShape() {
@@ -40,20 +77,31 @@ public class EventSquare implements OnTouchListener{
 		this.event = event;
 	}
 
-	@Override
-	public boolean onTouch(View arg0, MotionEvent arg1) {
+
+	
+	
+	public boolean handleTouch(View arg0, MotionEvent arg1) {
 		// TODO Auto-generated method stub
 		switch(arg1.getAction()){
 		case MotionEvent.ACTION_DOWN: {
 			float posX = arg1.getX();
 			float posY = arg1.getY();
-			if(posX >= this.x && posX <= this.width && posY >= this.y && posY <= this.height){
+			if(posX >= this.x1 && posX <= this.x2 && posY >= this.y1 && posY <= this.y2){
 				System.out.println("Ping");
+				return true;
+			}
+			else{
+				return false;//allow other listeners to work
+
 			}
 		}
+		default:
+			return false;//allow other listeners to work
+
+
 		}
-		return true;
+
 	}
-	
-	
+
+
 }
